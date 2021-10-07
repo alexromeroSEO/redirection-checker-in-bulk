973e5b1f8d4d6d612dc6d90610ea6a94fd2b48e6
Romero <Alejandro.Romero@reprisedigital.com>
2021-10-07 08:45:53 +0200

    rewrite code

--- main.js
+++ main.js
@@ -1,57 +1,44 @@
 function RedirectChecker() {
-<<<<<<< HEAD
-    HOLA HOLA
-=======
->>>>>>> d57f81c3eb202836ec81ab0fb4b54e6d65d46e17
-    var ss = SpreadsheetApp.getActiveSpreadsheet();
-    var ufila = ss.getLastRow();
-    i = 3;
-    var urls = ss
-                      .getRange("B3:B" + ufila)
-                      .getValues()
-                      .flat();
-    options = {
-      'method': 'post', 
-      'muteHttpExceptions': true, 
-      'followRedirects': false
-    }
-    
-    const iterator = urls.forEach(url => {
-      var IdealUrl = ss.getRange("C"+i+":C"+i).getValues() 
-      var response = UrlFetchApp.fetch(url, options );
-      var responseCode = response.getResponseCode()
-      var redirect = /^3.+/.test(responseCode);
-      if(redirect){
-        ss.getRange("D"+i+":D"+i).setValue('HTTP '+responseCode)
-        var FinalURL = response.getHeaders().Location;
-        ss.getRange("E"+i+":E"+i).setValue(FinalURL)
-          if(FinalURL == IdealUrl){
-          ss.getRange("F"+i+":F"+i).setValue("Si")  
-          }else{
-          ss.getRange("F"+i+":F"+i).setValue("No") 
-          var response2 = UrlFetchApp.fetch(FinalURL, options);
-          var FinalURL2 = response2.getHeaders().Location;
-            if(FinalURL2 != FinalURL){
-              ss.getRange("G"+i+":G"+i).setValue(FinalURL2)
-              ss.getRange("H"+i+":H"+i).setValue(FinalURL2 == IdealUrl ? "Si" : "No")
-<<<<<<< HEAD
-          }else{}
-=======
-          }else{} 
->>>>>>> d57f81c3eb202836ec81ab0fb4b54e6d65d46e17
-        }
-      }else{
-        ss.getRange("D"+i+":D"+i).setValue('HTTP '+responseCode)
-      }
-      i++
-      }
-      )
+  var ss = SpreadsheetApp.getActiveSpreadsheet();
+  var ufila = ss.getLastRow();
+  i = 3;
+  var urls = ss
+                    .getRange("B3:B" + ufila)
+                    .getValues()
+                    .flat();
+  options = {
+    'method': 'post', 
+    'muteHttpExceptions': true,
+    'followRedirects': false
   }
-    
   
+  const iterator = urls.forEach(url => {
+    var IdealUrl = ss.getRange("C"+i+":C"+i).getValues() 
+    var response = UrlFetchApp.fetch(url, options );
+    var responseCode = response.getResponseCode()
+    var redirect = /^3.+/.test(responseCode);
+    if(redirect){
+      ss.getRange("D"+i+":D"+i).setValue('HTTP '+responseCode)
+      var FinalURL = response.getHeaders().Location;
+      ss.getRange("E"+i+":E"+i).setValue(FinalURL)
+        if(FinalURL == IdealUrl){
+        ss.getRange("F"+i+":F"+i).setValue("Si")  
+        }else{
+        ss.getRange("F"+i+":F"+i).setValue("No") 
+        var response2 = UrlFetchApp.fetch(FinalURL, options);
+        var FinalURL2 = response2.getHeaders().Location;
+          if(FinalURL2 != FinalURL){
+            ss.getRange("G"+i+":G"+i).setValue(FinalURL2)
+            ss.getRange("H"+i+":H"+i).setValue(FinalURL2 == IdealUrl ? "Si" : "No")
+        }else{}
+      }
+    }else{
+      ss.getRange("D"+i+":D"+i).setValue('HTTP '+responseCode)
+    }
+    i++
+    }
+    )
+}
   
-<<<<<<< HEAD
-  
-=======
-  
->>>>>>> d57f81c3eb202836ec81ab0fb4b54e6d65d46e17
+
+
